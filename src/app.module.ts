7109ddf4851b2e9dd5aebd2874c866167c2341ff
emanuel-o-dev <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-09-14 19:51:48 -0300

    Add CoreModule and SharedModule; update AppModule imports and configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 @Module({
-  imports: [],
+  imports: [SharedModule, CoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
+// O que faz esse módulo?
+// Este módulo é o módulo raiz da aplicação, onde todos os outros módulos são importados e configurados.
 export class AppModule {}
+// O que é um módulo?
+// Um módulo é uma classe anotada com o decorator @Module, que organiza o código em unidades coesas, agrupando controladores, provedores e outros módulos relacionados.
