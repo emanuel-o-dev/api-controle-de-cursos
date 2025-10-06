b97cadb03b8b7afb5240f40d9f406df7722fbc67
emanuel-o-dev <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-10-05 23:10:57 -0300

    Add logging and authentication middlewares with corresponding tests

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggerMiddleware } from './middlewares/logger/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.use(new LoggerMiddleware().use.bind(new LoggerMiddleware())); // Aplica o middleware globalmente
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
