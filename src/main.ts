dba39be41c9717ee0f1d0a13a58061da1513abb7
emanuel-o-dev <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-10-30 18:55:13 -0300

    Add Swagger configuration and setup for API documentation

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { LoggerMiddleware } from './middlewares/logger/logger.middleware';
 import { HttpExceptionFilter } from './filters/http-exception/http-exception.filter';
 import { ResponseInterceptor } from './interceptors/response/response.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,9 +17,24 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  app.use(new LoggerMiddleware().use); // Aplica o middleware globalmente
+  app.use(new LoggerMiddleware().use);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new ResponseInterceptor());
+
+  // Configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API com Swagger')
+    .setDescription('Documentação automática da API com Swagger')
+    .setVersion('1.0')
+    .addBearerAuth() // Para habilitar autenticação JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // Gera o arquivo swagger.json
+  //writeFileSync('./swagger.json', JSON.stringify(document, null, 2));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
