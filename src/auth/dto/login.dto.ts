2a5446d28c4c9804514ff22e64dfe77305f2968b
emanuel-o-dev <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-10-30 19:21:21 -0300

    Update LoginDto examples for email and password fields for improved clarity

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,9 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
-  @ApiProperty({ example: 'joao@email.com' })
+  @ApiProperty({ example: 'testeStudio@example.com' })
   email: string;
 
-  @ApiProperty({ example: '12345678' })
+  @ApiProperty({ example: '123456' })
   password: string;
 }
