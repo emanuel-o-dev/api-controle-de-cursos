cec0372ab36a231e7cc8448dce786702361b4107
emanuel-o-dev <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-10-30 19:07:54 -0300

    Enhance CreateUserDto and UsersController with Swagger documentation for improved API clarity and user role management

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,13 +6,18 @@ import {
   Param,
   ParseIntPipe,
   Body,
-  Post,
   Delete,
   Put,
   UseGuards,
   ForbiddenException,
   Request,
 } from '@nestjs/common';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiBearerAuth,
+  ApiResponse,
+} from '@nestjs/swagger';
 import { HttpExceptionFilter } from 'src/filters/http-exception/http-exception.filter';
 import { ResponseInterceptor } from 'src/interceptors/response/response.interceptor';
 import { UsersService } from './users.service';
@@ -23,26 +28,43 @@ import { Roles } from 'src/auth/decorators/roles.decorator';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
+@ApiTags('Users')
 @Controller('users')
 @UseFilters(HttpExceptionFilter)
 @UseInterceptors(ResponseInterceptor)
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
+  @ApiOperation({ summary: 'Buscar usuário por ID' })
+  @ApiResponse({ status: 200, description: 'Usuário encontrado com sucesso.' })
+  @ApiResponse({ status: 404, description: 'Usuário não encontrado.' })
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.userService.findOneById(id);
   }
 
+  @ApiBearerAuth()
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.ADMIN, Role.INSTRUCTOR)
+  @ApiOperation({ summary: 'Listar todos os usuários' })
+  @ApiResponse({
+    status: 200,
+    description: 'Lista de usuários retornada com sucesso.',
+  })
   @Get()
   findAll() {
     return this.userService.findAll();
   }
 
+  @ApiBearerAuth()
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.ADMIN, Role.INSTRUCTOR, Role.USER)
+  @ApiOperation({ summary: 'Atualizar dados de um usuário' })
+  @ApiResponse({ status: 200, description: 'Usuário atualizado com sucesso.' })
+  @ApiResponse({
+    status: 403,
+    description: 'Usuário não autorizado para atualizar este perfil.',
+  })
   @Put(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
@@ -55,8 +77,12 @@ export class UsersController {
     return this.userService.update(id, dto);
   }
 
+  @ApiBearerAuth()
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.ADMIN, Role.INSTRUCTOR, Role.USER)
+  @ApiOperation({ summary: 'Remover usuário por ID' })
+  @ApiResponse({ status: 200, description: 'Usuário removido com sucesso.' })
+  @ApiResponse({ status: 404, description: 'Usuário não encontrado.' })
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.userService.remove(id);
