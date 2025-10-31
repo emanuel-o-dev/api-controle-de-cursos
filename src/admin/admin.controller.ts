e8e90ce95a56a7f311a20a1ef8cfc1d5afc26f25
emanuel-o-dev <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-10-31 19:27:55 -0300

    Enhance AdminController with Swagger documentation for improved API clarity and authentication handling

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -2,12 +2,16 @@ import { UseGuards, Controller, Get } from '@nestjs/common';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
 
 @Controller('admin')
 @UseGuards(JwtAuthGuard, RolesGuard)
 export class AdminController {
   @Get()
   @Roles('ADMIN')
+  @ApiResponse({ status: 200, description: 'Acesso concedido ao admin.' })
+  @ApiResponse({ status: 401, description: 'Acesso negado.' })
+  @ApiBearerAuth()
   getAdminData() {
     return { message: 'Bem-vindo, Admin!' };
   }
