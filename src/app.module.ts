f159e0a8b4afff45a246616835c34eadfe9f4411
emanuel-o-dev <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-11-02 20:14:38 -0300

    Refactor AppModule to remove unused MiddlewareConsumer import and simplify configure method; update import statement in e2e test for consistency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { CoursesModule } from './courses/courses.module';
 import { UsersModule } from './users/users.module';
-import { AuthMiddleware } from './middlewares/auth/auth.middleware';
 import { ErrorsModule } from './errors/errors.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
@@ -29,5 +28,5 @@ import { AdminModule } from './admin/admin.module';
 // O que faz esse módulo?
 // Este módulo é o módulo raiz da aplicação, onde todos os outros módulos são importados e configurados.
 export class AppModule {
-  configure(consumer: MiddlewareConsumer) {}
+  configure() {}
 }
